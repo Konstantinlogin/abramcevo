48880170ea37ec32a0dd55f838a19cda44f3c189
Константин Елагин <elagin.uk@yandex.ru>
2017-06-29 23:57:50 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    var menuNameText = $('.menu-button-block__menu-name').text();
+
+    $('.menu-btn, .menu-button-block__menu-name').on('click', function (event) {
+        
+        event.preventDefault();
+
+        $('.menu-btn').toggleClass('is-open');
+
+        if ($('.menu-btn').hasClass('is-open')) {
+
+            $(".menu-button-block")
+                .addClass('is-active');
+            $(".nav")
+                .addClass('is-active');
+            $(".menu-button-block__menu-name")
+                .text("закрыть")
+                .addClass("is-active");
+
+        }
+
+        else {
+            $(".menu-button-block")
+                .removeClass('is-active');  // Todo: сделать удаление класса с .menu-button-block через setinterval, равным transition анимации закрытия блока .nav
+            $(".nav").removeClass('is-active')
+            $(".menu-button-block__menu-name")
+                .text(menuNameText)
+                .removeClass("is-active");
+
+        }
+
+    });
+
+
+
+});
