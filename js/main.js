024fccddbb4503e78f8a8f0a55309c5e9b661fe5
Konstantin Elagin <k.elagin@sape.ru>
2017-07-24 14:40:38 +0300

    ch

--- js/main.js
+++ js/main.js
@@ -49,6 +49,12 @@ $(document).ready(function () {
         });
     } 
 
+    $(".scroll-to-bottom").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 500);
+    });
 
     // ++++ Inits
 
