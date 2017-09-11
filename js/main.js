a6714b9e3e2b018f324ed1f874e903bdd20cdce1
Константин Елагин <elagin.uk@yandex.ru>
2017-09-11 23:47:20 +0300

    ch

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ var isIOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 
 $(document).ready(function () {
 
+
+
     var menuNameText = $('.menu-button-block__menu-name').text();
     // TODO: хуёвые селекторы, да и вообще можно лаконичнее все написать. Похуй. 
     var isMenuActive = false;
-    
+
     $('.menu-btn, .menu-button-block__menu-name').on('click', function (event) {
         event.preventDefault();
         $('.menu-btn').toggleClass('is-open');
@@ -28,7 +30,7 @@ $(document).ready(function () {
     }
 
     function closeMenu() {
-        
+
         $(".nav")
             .removeClass('is-active');
         setTimeout(function () {
@@ -40,8 +42,8 @@ $(document).ready(function () {
     }
 
     function closeMenuOnBodyClick() {
-        $(document).click(function(event) {
-            if(!$(event.target).closest('.nav').length && !$(event.target).closest('.menu-button-block').length) {
+        $(document).click(function (event) {
+            if (!$(event.target).closest('.nav').length && !$(event.target).closest('.menu-button-block').length) {
                 closeMenu();
                 $('.menu-btn').removeClass('is-open');
             }
@@ -50,14 +52,14 @@ $(document).ready(function () {
 
     $(".scroll-to-bottom").on("click", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
-            $('body,html').animate({scrollTop: top}, 500);
+        $('body,html').animate({ scrollTop: top }, 500);
     });
 
-    $('.search').find('.search__input').bind('keyup paste change', function(){
+    $('.search').find('.search__input').bind('keyup paste change', function () {
         var isEmpty = true;
-        if(isEmpty = true && $.trim( this.value ) === "") {
+        if (isEmpty = true && $.trim(this.value) === "") {
             isEmpty = false;
         }
         else {
@@ -75,6 +77,144 @@ $(document).ready(function () {
         }
     }
 
- 
+    // INITS +++++++++++==
+
+    var slider = {
+        services: '.slider-services',
+    };
+
+    var yandexMap = {
+        main: '#yandexMapMainPage',
+        contacts: '#yandexMapContacts'
+    }
+
+    var video = '#bgVideo';
+    // bvideo.js
+
+    if ($(video).length > 0) {
+        console.log(1)
+
+        var bv = new Bideo();
+
+        bv.init({
+            // Video element
+            videoEl: document.querySelector('#background_video'),
+
+            // Container element
+            container: document.querySelector('body'),
+
+            // Resize
+            resize: true,
+
+            // autoplay: false,
+
+            // isMobile: window.matchMedia('(max-width: 768px)').matches,
+            // show/hide on mobile
+
+            playButton: document.querySelector('#play'),
+            pauseButton: document.querySelector('#pause'),
+
+            // Array of objects containing the src and type
+            // of different video formats to add
+            src: [
+                {
+                    src: 'video/video6.mp4',
+                    type: 'video/mp4'
+                }
+            ],
+
+            // What to do once video loads (initial frame)
+            onLoad: function () {
+                $('#video_cover').hide()
+                $('#background_video_preloader').hide();
+            }
+        });
+
+
+        if (isIOS == true) {
+            $('#background_video_preloader').hide();
+        }
+
+    }
+
+
+    if ($(slider.services).length > 0) {
+        var servicesSlider = new Swiper(slider.services, {
+            nextButton: '.slider-services__arrow-right',
+            prevButton: '.slider-services__arrow-left',
+            slideClass: 'slider-services__slide',
+            autoplay: 4000,
+            loop: true,
+            spaceBetween: 30
+        });
+
+    }
+
+    if ($(yandexMap.main).length > 0) {
+        // Yandex map
+        ymaps.ready(init);
+        function init() {
+            var myMap;
+            var myPlacemark;
+            var adress;
+
+            ymaps.geocode(adress).then(function (res) {
+                adress = [56.243523, 37.936712];
+                myMap = new ymaps.Map("yandexMapMainPage", {
+                    center: adress,
+                    zoom: 15,
+                    // controls: ['zoomControl']
+                    controls: []
+                });
+
+                myMap.behaviors.disable(['drag', 'scrollZoom', 'rightMouseButtonMagnifier']);
+
+                myPlacemark = new ymaps.Placemark(adress, {
+                    // hintContent: 'Можно написать хинт',
+                    balloonContent: 'КСК Абрамцево'
+                });
+
+                myMap.geoObjects.add(myPlacemark);
+            }
+
+            );
+
+
+        }
+    }
+
+
+    if ($(yandexMap.contacts.length > 0)) {
+        ymaps.ready(initContacts);
+        function initContacts() {
+            var mapContacts;
+            var placemarkContacts;
+            var adressContacts;
+
+            ymaps.geocode(adressContacts).then(function (res) {
+                adressContacts = [56.243523, 37.936712];
+                mapContacts = new ymaps.Map("yandexMapContacts", {
+                    center: adressContacts,
+                    zoom: 15,
+                    controls: ['zoomControl']
+                    // controls: []
+                });
+
+                // myMap.behaviors.disable(['drag', 'scrollZoom',  'rightMouseButtonMagnifier']);
+
+                placemarkContacts = new ymaps.Placemark(adress, {
+                    // hintContent: 'Можно написать хинт',
+                    balloonContent: 'КСК Абрамцево'
+                });
+
+                mapContact.geoObjects.add(placemarkContacts);
+            }
+
+            );
+
+
+        }
+    }
+
 });
 
